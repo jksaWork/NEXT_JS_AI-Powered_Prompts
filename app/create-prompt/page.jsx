629a4be5058c01d14f890b094304b57913043fb2
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2023-07-20 03:59:37 -0700

    Fix The Small Case

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import Form from "@/components/Form";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-function page() {
+function Page() {
   // const
   const router = useRouter();
   const { data: session } = useSession();
@@ -59,4 +59,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
